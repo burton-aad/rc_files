5d7bc4871dfad7408627fcaeed3c6410ddb89c53
Alexandre Adolphe <alexandre.adolphe@gmail.com>
2019-03-11 22:12:51 +0100

    Separe l'install de paquets

---
+++ install_packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Install packages
+
+PKGS="git curl vim tofrodos rxvt-unicode-256color"
+
+if dpkg -l | grep -q xserver; then
+    # grapical display
+    PKGS="$PKGS gitk emacs"
+else
+    # Non graphical display
+    PKGS="$PKGS emacs-nox"
+fi
+
+sudo apt install -y $PKGS
